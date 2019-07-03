a8b46b72cbdbf609981a2d5acd3b73805491eb04
ZJN <31883409+southzjn@users.noreply.github.com>
2019-07-03 16:52:35 +0800

    数据库搭建完成，包含数据库操作API和服务器端路由管理代码

---
+++ server/mongo/api.js
@@ -0,0 +1,81 @@
+﻿var YiTaoFangModel = require('./mongodb');
+
+module.exports = {
+
+    /**
+     *  添加文档
+     *
+     * @param {*} data  数据 
+     * @param {*} model 模型
+     * @returns
+     */
+    createNew(data, model) {
+        return new Promise((resolve, reject) => {
+            YiTaoFangModel[model].create(data, (err, doc) => {
+                if(err) {
+                    reject(err)
+                }
+                else {
+                    resolve(doc)
+                }
+            })
+        })
+    },
+
+    /**
+     *  查找特定的文档(只返回一个)
+     *
+     * @param {*} data
+     * @param {*} model
+     * @returns
+     */
+    findOne(data, model) {
+        return new Promise((resolve, reject) => {
+            YiTaoFangModel[model].findOne(data, (error, doc) => {
+                if(error){
+                    reject(error)
+                }else{
+                    resolve(doc)
+                }
+            })
+        })
+    },
+
+    /**
+     *  查找符合条件的文档(返回一个数组)
+     *
+     * @param {*} data
+     * @returns
+     */
+    find(data, model) {
+        return new Promise((resolve, reject) => {
+            YiTaoFangModel[model].find(data, (error, doc) => {
+                if(error){
+                    reject(error)
+                }else{
+                    resolve(doc)
+                }
+            })
+        })
+    },
+
+
+    /**
+     *  更新文档信息(只更新一个)
+     *
+     * @param {*} conditions
+     * @param {*} update
+     * @returns
+     */
+    updateOne(conditions, update, model) {
+        return new Promise((resolve, reject) => {
+            YiTaoFangModel[model].updateOne(conditions, update, (error, doc) => {
+                if(error){
+                    reject(error)
+                }else{
+                    resolve(doc)
+                }
+            })
+        })
+    },
+}

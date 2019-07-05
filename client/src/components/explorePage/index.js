d32bb71857810e21676711fbc58d04536a88292b
ZJN <31883409+southzjn@users.noreply.github.com>
2019-07-05 17:17:45 +0800

    初步实现发现页

---
+++ client/src/components/explorePage/index.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+/**
+ *  发现页的组件
+ *
+ * @class ExplorePage
+ * @extends {Component}
+ */
+class ExplorePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                ExplorePage
+            </div>
+        )
+    }
+}
+
+export default ExplorePage;
